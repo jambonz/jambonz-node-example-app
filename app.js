f9851036486f8da881b3bb04dd3a139b9bfa5ea8
Dave Horton <daveh@beachdognet.com>
2021-08-12 19:10:35 -0400

    revamp attended transfer

--- app.js
+++ app.js
@@ -1,37 +1,42 @@
-require('dotenv').config();
+const assert = require('assert');
+assert.ok(process.env.JAMBONZ_ACCOUNT_SID, 'You must define the JAMBONZ_ACCOUNT_SID env variable');
+assert.ok(process.env.JAMBONZ_API_KEY, 'You must define the JAMBONZ_API_KEY env variable');
+assert.ok(process.env.JAMBONZ_REST_API_BASE_URL, 'You must define the JAMBONZ_REST_API_BASE_URL env variable');
+assert.ok(process.env.WS_RECORD_PATH, 'You must define the WS_RECORD_PATH env variable');
+
 const express = require('express');
 const app = express();
+const Websocket = require('ws');
 const {WebhookResponse} = require('@jambonz/node-client');
 const basicAuth = require('express-basic-auth');
 const opts = Object.assign({
-  timestamp: () => {
-    return `, "time": "${new Date().toISOString()}"`;
-  }
-}, {
+  timestamp: () => `, "time": "${new Date().toISOString()}"`,
   level: process.env.LOGLEVEL || 'info'
 });
 const logger = require('pino')(opts);
-const credentials = new Map();
 const {loadCredentials, calculateResponse} = require('./lib/utils')(logger);
 const port = process.env.HTTP_PORT || 3000;
 const routes = require('./lib/routes');
-
-Object.assign(app.locals, {
+app.locals = {
+  ...app.locals,
   logger,
-  credentials,
   calculateResponse,
   client: require('@jambonz/node-client')(process.env.JAMBONZ_ACCOUNT_SID, process.env.JAMBONZ_API_KEY, {
     baseUrl: process.env.JAMBONZ_REST_API_BASE_URL
   })
-});
+};
 
-/*
+/* set up a websocket server to receive audio from the 'listen' verb */
+const {recordAudio} = require('./lib/utils')(logger);
+logger.info(`setting up record path at ${process.env.WS_RECORD_PATH}`);
+const wsServer = new Websocket.Server({ noServer: true });
+wsServer.setMaxListeners(0);
+wsServer.on('connection', recordAudio.bind(null, logger));
 if (process.env.HTTP_USERNAME && process.env.HTTP_PASSWORD) {
   const users = {};
   users[process.env.HTTP_USERNAME] = process.env.HTTP_PASSWORD;
   app.use(basicAuth({users}));
 }
-*/
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.WEBHOOK_SECRET) {
@@ -43,8 +48,14 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).json({msg: err.message});
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   logger.info(`Example jambonz app listening at http://localhost:${port}`);
+});
+server.on('upgrade', (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    if (request.url !== process.env.WS_RECORD_PATH) return socket.destroy();
+    wsServer.emit('connection', socket, request);
+  });
 });
 
 const readSipCredentialsFromGoogleSheet = async(logger) => {
@@ -56,5 +67,6 @@ const readSipCredentialsFromGoogleSheet = async(logger) => {
   }
   setTimeout(readSipCredentialsFromGoogleSheet.bind(null, logger), 60000);
 };
-readSipCredentialsFromGoogleSheet(logger);
-
+if (process.env.GOOGLE_SHEET_ID && process.env.GOOGLE_SHEET_CREDENTIALS_FILE) {
+  readSipCredentialsFromGoogleSheet(logger);
+}
